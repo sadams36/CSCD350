e134119da332ee041e084767f84449a5ead2379f
surci <surci@10.104.185.206>
2021-10-25 11:01:25 -0700

    Stubbed Aircraft

--- cs350f21task3/src/cs350f21task3/Aircraft.java
+++ cs350f21task3/src/cs350f21task3/Aircraft.java
@@ -2,4 +2,33 @@ package cs350f21task3;
 
 public class Aircraft {
 
+	private String id;
+	private State state;
+	
+	public Aircraft(String id,State state)
+	{
+		this.id = id;
+		this.state = state;
+	}
+	
+	public String getID()
+	{
+		return "";
+	}
+	
+	public State getState()
+	{
+		return null;
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+	
+	public void update()
+	{
+		
+	}
+	
 }
