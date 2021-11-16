559e02cfe02d986e1798dfb4ce43f02b001effec
SamuelUrcino <surcino58@gmail.com>
2021-11-16 00:29:10 -0800

    A_Loader and OverlayBuilder dont compile yet. FIXXXX

--- TaskP1/src/atcsim/loader/A_Loader.java
+++ TaskP1/src/atcsim/loader/A_Loader.java
@@ -1,5 +1,10 @@
 package atcsim.loader;
 
 public abstract class A_Loader {
-
+	
+	public A_Loader(java.util.Map<java.lang.String,​A_ComponentNavaid<?>> navaids, OverlayNavigation overlay)
+	{
+		
+	}
+	
 }
