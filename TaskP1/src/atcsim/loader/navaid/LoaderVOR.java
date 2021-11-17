d33ee595dbf2ff7c067618627d500d5e0c24f644
surci <surci@192.168.56.1>
2021-11-16 20:12:53 -0800

    LoaderVOR constructor

--- TaskP1/src/atcsim/loader/navaid/LoaderVOR.java
+++ TaskP1/src/atcsim/loader/navaid/LoaderVOR.java
@@ -1,6 +1,6 @@
 package atcsim.loader.navaid;
-import java.util.Map;
 
+import java.util.Map;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
