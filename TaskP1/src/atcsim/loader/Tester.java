0c3b15a5c9ab4d850a680f4bed51c92a4f4cbf09
15098 <15098@10.104.177.51>
2021-11-19 11:51:29 -0800

    How to build/test a loader - Savannah

--- TaskP1/src/atcsim/loader/Tester.java
+++ TaskP1/src/atcsim/loader/Tester.java
@@ -1,11 +1,18 @@
 package atcsim.loader;
 
+import atcsim.world.navigation.A_ComponentNavaid;
+
 public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NavigationOverlayBuilder build = new NavigationOverlayBuilder();
 		
+		// Testing a Loader
+			// Step 1: build a Map object (pass it a string and an A_ComponentNavaid<?>, unsure what these are at this point)
+			// Step 2: build an OverlayNavigation object, pass it a string (the string represents an ID))
+			// Step 3: pass the above two objects into the appropriate loader constructor
+			// Step 4: print the new loader (assuming there's some sort of automatic toString)
 	}
 
 }
