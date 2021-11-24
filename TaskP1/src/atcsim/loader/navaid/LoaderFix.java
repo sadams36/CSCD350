6fe467167fb0ac4e0eb0b3711842d8cf2af78066
surci <surci@DESKTOP-OJU6JCS.mshome.net>
2021-11-23 18:00:25 -0800

    Implementations
    Added LoaderFix load method. Changed A_Loader visibility modifiers to
    protected

--- TaskP1/src/atcsim/loader/navaid/LoaderFix.java
+++ TaskP1/src/atcsim/loader/navaid/LoaderFix.java
@@ -3,9 +3,14 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
+import atcsim.datatype.Altitude;
+import atcsim.datatype.CoordinateWorld3D;
+import atcsim.datatype.Latitude;
+import atcsim.datatype.Longitude;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidFix;
 
 public class LoaderFix extends A_Loader{
 
@@ -16,7 +21,7 @@ public class LoaderFix extends A_Loader{
 	
 	/*
 	 * fix1, 48,38,31, 116,24,29, 1949
-	 * ID  , position or (longitude,latitude,altitude)
+	 * ID  , position or (latitude,longitude,altitude)
 	 */
 	@Override
 	public void load(Scanner scanner) throws IOException {
@@ -24,10 +29,48 @@ public class LoaderFix extends A_Loader{
 		if(scanner == null)
 			throw new IllegalArgumentException("Scanner is null");
 		
-		while(scanner.hasNextLine())
-		{
-			System.out.println(scanner.nextLine());//testing line grab for fix
-		}
+		boolean load = true;
+		
+		scanner.nextLine();
+		String lineToRead = scanner.nextLine();
+	
+			while(load)
+	        {
+	            //this split will have 4 sections that are based off of " ," in a line
+	            String[] split = lineToRead.split(", ");
+	            
+	            String id = split[0];
+	            System.out.println(id);
+	            
+	            //used to make a latitude object
+	            String[] latSplit = split[1].split(",");
+	            System.out.println(latSplit[0] + " " +latSplit[1]+ " " +latSplit[2]);
+	            Latitude latitude = new Latitude(Integer.parseInt(latSplit[0]),Integer.parseInt(latSplit[1]),Double.parseDouble(latSplit[2])); 
+	            
+	            //used to make longitude object
+	            String[] longSplit = split[2].split(",");
+	            System.out.println(longSplit[0] + " " +longSplit[1]+ " " +longSplit[2]);
+	            Longitude longitude = new Longitude(Integer.parseInt(longSplit[0]),Integer.parseInt(longSplit[1]),Double.parseDouble(longSplit[2]));
+	            
+	          
+	            //since altitude only has one element in that array we just assign
+	            //used to make altitude object
+	            String altSplit = split[3];
+	            System.out.println(altSplit);
+	            Altitude altitude = new Altitude(Double.parseDouble(altSplit));
+
+	            CoordinateWorld3D coordinate = new CoordinateWorld3D(latitude,longitude,altitude);
+	            ComponentNavaidFix fix = new ComponentNavaidFix(id,coordinate); 
+	            
+	            this.navaids.put(split[0].trim(), fix);
+	            
+	            lineToRead = scanner.nextLine();
+	            		
+	            if(lineToRead.isBlank() || lineToRead.isEmpty())
+	            		load = false;
+	            
+	        }
+		
 	}
 
 }
