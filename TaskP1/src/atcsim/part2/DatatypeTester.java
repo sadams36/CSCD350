bd12ec8c0da3829acd8f4d173c6bc4054862e2cc
SamuelUrcino <surcino58@gmail.com>
2021-11-30 22:57:39 -0800

    About 30% tests done for altitude

--- TaskP1/src/atcsim/part2/DatatypeTester.java
+++ TaskP1/src/atcsim/part2/DatatypeTester.java
@@ -4,11 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import atcsim.datatype.Altitude;
+
+//API https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+
 class DatatypeTester {
 
 	@Test
 	public void testAltitude() {
-		fail("Not yet implemented");
+		
+		Altitude testAlt = new Altitude(1000);
+		Altitude testAlt2 = new Altitude(200);
+		
+		//assertEquals(Expected Result(We set this),Actual Result(The result that is calculated))
+		
+		//alt1 + alt2
+		double result = testAlt.getValue_() + testAlt2.getValue_();
+		assertEquals(1200, result,"from result");
+		
+		//alt2 + alt1
+		double result2 = testAlt2.getValue_() + testAlt.getValue_();
+		assertEquals(1200, result2,"from result");
+		
+		
 	}
 	
 	@Test
