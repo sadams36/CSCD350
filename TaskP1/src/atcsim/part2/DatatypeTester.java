133a2b72ea6a6855206c3769b470abdecea6f0b7
SamuelUrcino <surcino58@gmail.com>
2021-11-30 21:58:32 -0800

    Stubbed test methods

--- TaskP1/src/atcsim/part2/DatatypeTester.java
+++ TaskP1/src/atcsim/part2/DatatypeTester.java
@@ -7,8 +7,44 @@ import org.junit.jupiter.api.Test;
 class DatatypeTester {
 
 	@Test
-	void test() {
+	public void testAltitude() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testAngleNavigational() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAttitudePitch() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAttitudeYaw() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCourse() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCoordinateWorld() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCoordinateWorld3D() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+		
+	
+	
 
 }
