1aec29d1371021d51487e2d33d6e8ecddd30a189
SamuelUrcino <surcino58@gmail.com>
2021-11-30 21:48:33 -0800

    made tester class

---
+++ TaskP1/src/atcsim/part2/DatatypeTester.java
@@ -0,0 +1,14 @@
+package atcsim.part2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DatatypeTester {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
